c1c081415f64c79a557a5003eb2120aa81054087
13-Rob <rcgr.13@gmail.com>
2023-04-05 21:58:01 -0700

    Namespaces and Invoke

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 0.5f;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Ground") {
-            Debug.Log("Bonk");
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 2f;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player") {
-            Debug.Log("Finish Line");
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
